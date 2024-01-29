c093bad288fec150923de1ddba4b97b4540389ee
Jeremy <deletelater@CEO-of-Brain-Damage.local>
2024-01-28 20:18:18 -0500

    Main Page Blocked

---
+++ Call of Cthulhu Character Builder/src/MainPage.tsx
@@ -0,0 +1,18 @@
+import "./MainPage.css";
+
+function MainPage() {
+  return (
+    <>
+      <header>
+        <title>Call of Cthulhu Character Creator</title>
+      </header>
+      <h1>Call of Cthulhu Character Creator</h1>
+      <h2 className="New">New Character</h2>
+      <h2 className="Load">Load Character</h2>
+      <div className="NewSquare"></div>
+      <div className="LoadSquare"></div>
+    </>
+  );
+}
+
+export default MainPage;
