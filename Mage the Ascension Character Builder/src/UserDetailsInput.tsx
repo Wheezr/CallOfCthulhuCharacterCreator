840f44137a4ea43551e7cabb955cb46dd822b579
Jerry Cheng <chengj12@rpi.edu>
2024-03-18 18:23:38 -0400

    Update UserDetailsInput to have all the characteristics and fix some bugs

--- Mage the Ascension Character Builder/src/UserDetailsInput.tsx
+++ Mage the Ascension Character Builder/src/UserDetailsInput.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, CSSProperties } from "react";
 
 const UserDetailsInput = () => {
   const [userDetails, setUserDetails] = useState({
     name: "",
+    player: "",
     nature: "",
     essence: "",
     demeanor: "",
@@ -21,6 +22,7 @@ const UserDetailsInput = () => {
 
   return (
     <div style={styles.container}>
+      {/* Name */}
       <label htmlFor="name">Name:</label>
       <input
         style={styles.input}
@@ -30,7 +32,17 @@ const UserDetailsInput = () => {
         id="name"
       />
 
-      {/* Repeat the pattern for each field */}
+      {/* Player */}
+      <label htmlFor="player">Player:</label>
+      <input
+        style={styles.input}
+        onChange={(e) => handleInputChange("player", e.target.value)}
+        value={userDetails.player}
+        placeholder="Enter your player"
+        id="player"
+      />
+
+      {/* Nature */}
       <label htmlFor="nature">Nature:</label>
       <input
         style={styles.input}
@@ -40,13 +52,70 @@ const UserDetailsInput = () => {
         id="nature"
       />
 
-      {/* Continue for each input... */}
-      {/* Essence, Demeanor, Tradition, Concept, Chronicle, Cabal */}
+      {/* Essence */}
+      <label htmlFor="essence">Essence:</label>
+      <input
+        style={styles.input}
+        onChange={(e) => handleInputChange("essence", e.target.value)}
+        value={userDetails.essence}
+        placeholder="Enter your essence"
+        id="essence"
+      />
+
+      {/* Demeanor */}
+      <label htmlFor="demeanor">Demeanor:</label>
+      <input
+        style={styles.input}
+        onChange={(e) => handleInputChange("demeanor", e.target.value)}
+        value={userDetails.demeanor}
+        placeholder="Enter your demeanor"
+        id="demeanor"
+      />
+
+      {/* Tradition */}
+      <label htmlFor="tradition">Tradition:</label>
+      <input
+        style={styles.input}
+        onChange={(e) => handleInputChange("tradition", e.target.value)}
+        value={userDetails.tradition}
+        placeholder="Enter your tradition"
+        id="tradition"
+      />
+
+      {/* Concept */}
+      <label htmlFor="concept">Concept:</label>
+      <input
+        style={styles.input}
+        onChange={(e) => handleInputChange("concept", e.target.value)}
+        value={userDetails.concept}
+        placeholder="Enter your concept"
+        id="concept"
+      />
+
+      {/* Chronicle */}
+      <label htmlFor="chronicle">Chronicle:</label>
+      <input
+        style={styles.input}
+        onChange={(e) => handleInputChange("chronicle", e.target.value)}
+        value={userDetails.chronicle}
+        placeholder="Enter your chronicle"
+        id="chronicle"
+      />
+
+      {/* Cabal */}
+      <label htmlFor="cabal">Cabal:</label>
+      <input
+        style={styles.input}
+        onChange={(e) => handleInputChange("cabal", e.target.value)}
+        value={userDetails.cabal}
+        placeholder="Enter your cabal"
+        id="cabal"
+      />
     </div>
   );
 };
 
-const styles = {
+const styles: { container: CSSProperties; input: CSSProperties } = {
   container: {
     display: "flex",
     flexDirection: "column",
