91d649cc3bb9389d68412b869f2479db1cbde02d
Jeremy <jeremy1202chen@gmail.com>
2024-03-18 15:37:46 -0400

    Proper state storage and component changing

--- Mage the Ascension Character Builder/src/Attributes/Physical.tsx
+++ Mage the Ascension Character Builder/src/Attributes/Physical.tsx
@@ -2,41 +2,63 @@ import "./Physical.css";
 import Dot from "../Dot";
 import { useState } from "react";
 import "../Dot.css"
+import produce from "immer";
 
-function Physical() {
-  const [strength, setStrength] = useState([true, false, false, false]);
+let strengthInit = [
+  'dot', 'undot', 'undot', 'undot'
+];
+
+let dexterityInit = [
+  'dot', 'dot', 'undot', 'undot'
+];
+
+export default function Physical() {
+  const [strengths, setStrength] = useState(strengthInit);
   const [dexterity, setDexterity] = useState([true, false, false, false]);
   const [stamina, setStamina] = useState([false, false, false, false]);
-  
+
+  function pressDot(index: number, array: string[]) {
+    const dot = array.map((c, i) => {
+      if (i === index && c === 'dot') {
+        return 'undot';
+      } else if (i === index && c === 'undot') {
+        return 'dot';
+      } else {
+        return c;
+      }
+    });
+    setStrength(dot);
+  }
+
   return (
-  <>
-    <h1>Physical</h1>
-    <div className="container">
-      <h2>Strength</h2>
-      <hr></hr>
-      <span className="dot"></span>
-      <div style={{display: "inline-block"}}>
-        {strength.map((item, index) => <Dot key={index} dotState={item} onClick={console.log(index)}></Dot>)}
+    <>
+      <h1>Physical</h1>
+      <div className="container">
+        <h2>Strength</h2>
+        <hr></hr>
+        <span className="dot"></span>
+        <div style={{display: "inline-block"}}>
+          {strengths.map((item, index) => <span className={item} onClick={() =>
+            pressDot(index, strengths)
+          }></span>)}
+        </div>
       </div>
-    </div>
-    <div className="container">
-      <h2>Dexterity</h2>
-      <hr></hr>
-      <span className="dot"></span>
-      <div style={{display: "inline-block"}}>
-        {dexterity.map((item, index) => <Dot key={index} dotState={item} onClick={console.log(index)}></Dot>)}
+      {/* <div className="container">
+        <h2>Dexterity</h2>
+        <hr></hr>
+        <span className="dot"></span>
+        <div style={{display: "inline-block"}}>
+          {dexterity.map((item, index) => <Dot key={index} dotState={item} onClick={console.log(index)}></Dot>)}
+        </div>
       </div>
-    </div>
-    <div className="container">
-      <h2>Stamina</h2>
-      <hr></hr>
-      <span className="dot"></span>
-      <div style={{display: "inline-block"}}> 
-        {stamina.map((item, index) => <Dot key={index} dotState={item} onClick={console.log(index)}></Dot>)}
-      </div>
-    </div>
-  </>
+      <div className="container">
+        <h2>Stamina</h2>
+        <hr></hr>
+        <span className="dot"></span>
+        <div style={{display: "inline-block"}}> 
+          {stamina.map((item, index) => <Dot key={index} dotState={item} onClick={console.log(index)}></Dot>)}
+        </div>
+      </div> */}
+    </>
   )
 };
-
-export default Physical;
