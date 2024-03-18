9f5611ce8b26b6da81cfb54700d2a80e40876fe1
Jerry Cheng <chengj12@rpi.edu>
2024-03-18 19:26:41 -0400

    Made UserDetailsInput component into a 3 by 3 grid

--- Mage the Ascension Character Builder/src/UserDetailsInput.tsx
+++ Mage the Ascension Character Builder/src/UserDetailsInput.tsx
@@ -22,113 +22,146 @@ const UserDetailsInput = () => {
 
   return (
     <div style={styles.container}>
-      {/* Name */}
-      <label htmlFor="name">Name:</label>
-      <input
-        style={styles.input}
-        onChange={(e) => handleInputChange("name", e.target.value)}
-        value={userDetails.name}
-        placeholder="Enter your name"
-        id="name"
-      />
+      <div style={styles.grid}>
+        {/* Name */}
+        <div style={styles.inputGroup}>
+          <label htmlFor="name">Name:</label>
+          <input
+            style={styles.input}
+            onChange={(e) => handleInputChange("name", e.target.value)}
+            value={userDetails.name}
+            placeholder="Enter your name"
+            id="name"
+          />
+        </div>
 
-      {/* Player */}
-      <label htmlFor="player">Player:</label>
-      <input
-        style={styles.input}
-        onChange={(e) => handleInputChange("player", e.target.value)}
-        value={userDetails.player}
-        placeholder="Enter your player"
-        id="player"
-      />
+        {/* Player */}
+        <div style={styles.inputGroup}>
+          <label htmlFor="player">Player:</label>
+          <input
+            style={styles.input}
+            onChange={(e) => handleInputChange("player", e.target.value)}
+            value={userDetails.player}
+            placeholder="Enter your player"
+            id="player"
+          />
+        </div>
 
-      {/* Nature */}
-      <label htmlFor="nature">Nature:</label>
-      <input
-        style={styles.input}
-        onChange={(e) => handleInputChange("nature", e.target.value)}
-        value={userDetails.nature}
-        placeholder="Enter your nature"
-        id="nature"
-      />
+        {/* Nature */}
+        <div style={styles.inputGroup}>
+          <label htmlFor="nature">Nature:</label>
+          <input
+            style={styles.input}
+            onChange={(e) => handleInputChange("nature", e.target.value)}
+            value={userDetails.nature}
+            placeholder="Enter your nature"
+            id="nature"
+          />
+        </div>
 
-      {/* Essence */}
-      <label htmlFor="essence">Essence:</label>
-      <input
-        style={styles.input}
-        onChange={(e) => handleInputChange("essence", e.target.value)}
-        value={userDetails.essence}
-        placeholder="Enter your essence"
-        id="essence"
-      />
+        {/* Essence */}
+        <div style={styles.inputGroup}>
+          <label htmlFor="essence">Essence:</label>
+          <input
+            style={styles.input}
+            onChange={(e) => handleInputChange("essence", e.target.value)}
+            value={userDetails.essence}
+            placeholder="Enter your essence"
+            id="essence"
+          />
+        </div>
 
-      {/* Demeanor */}
-      <label htmlFor="demeanor">Demeanor:</label>
-      <input
-        style={styles.input}
-        onChange={(e) => handleInputChange("demeanor", e.target.value)}
-        value={userDetails.demeanor}
-        placeholder="Enter your demeanor"
-        id="demeanor"
-      />
+        {/* Demeanor */}
+        <div style={styles.inputGroup}>
+          <label htmlFor="demeanor">Demeanor:</label>
+          <input
+            style={styles.input}
+            onChange={(e) => handleInputChange("demeanor", e.target.value)}
+            value={userDetails.demeanor}
+            placeholder="Enter your demeanor"
+            id="demeanor"
+          />
+        </div>
 
-      {/* Tradition */}
-      <label htmlFor="tradition">Tradition:</label>
-      <input
-        style={styles.input}
-        onChange={(e) => handleInputChange("tradition", e.target.value)}
-        value={userDetails.tradition}
-        placeholder="Enter your tradition"
-        id="tradition"
-      />
+        {/* Tradition */}
+        <div style={styles.inputGroup}>
+          <label htmlFor="tradition">Tradition:</label>
+          <input
+            style={styles.input}
+            onChange={(e) => handleInputChange("tradition", e.target.value)}
+            value={userDetails.tradition}
+            placeholder="Enter your tradition"
+            id="tradition"
+          />
+        </div>
 
-      {/* Concept */}
-      <label htmlFor="concept">Concept:</label>
-      <input
-        style={styles.input}
-        onChange={(e) => handleInputChange("concept", e.target.value)}
-        value={userDetails.concept}
-        placeholder="Enter your concept"
-        id="concept"
-      />
+        {/* Concept */}
+        <div style={styles.inputGroup}>
+          <label htmlFor="concept">Concept:</label>
+          <input
+            style={styles.input}
+            onChange={(e) => handleInputChange("concept", e.target.value)}
+            value={userDetails.concept}
+            placeholder="Enter your concept"
+            id="concept"
+          />
+        </div>
 
-      {/* Chronicle */}
-      <label htmlFor="chronicle">Chronicle:</label>
-      <input
-        style={styles.input}
-        onChange={(e) => handleInputChange("chronicle", e.target.value)}
-        value={userDetails.chronicle}
-        placeholder="Enter your chronicle"
-        id="chronicle"
-      />
+        {/* Chronicle */}
+        <div style={styles.inputGroup}>
+          <label htmlFor="chronicle">Chronicle:</label>
+          <input
+            style={styles.input}
+            onChange={(e) => handleInputChange("chronicle", e.target.value)}
+            value={userDetails.chronicle}
+            placeholder="Enter your chronicle"
+            id="chronicle"
+          />
+        </div>
 
-      {/* Cabal */}
-      <label htmlFor="cabal">Cabal:</label>
-      <input
-        style={styles.input}
-        onChange={(e) => handleInputChange("cabal", e.target.value)}
-        value={userDetails.cabal}
-        placeholder="Enter your cabal"
-        id="cabal"
-      />
+        {/* Cabal */}
+        <div style={styles.inputGroup}>
+          <label htmlFor="cabal">Cabal:</label>
+          <input
+            style={styles.input}
+            onChange={(e) => handleInputChange("cabal", e.target.value)}
+            value={userDetails.cabal}
+            placeholder="Enter your cabal"
+            id="cabal"
+          />
+        </div>
+      </div>
     </div>
   );
 };
 
-const styles: { container: CSSProperties; input: CSSProperties } = {
+const styles: {
+  container: CSSProperties;
+  grid: CSSProperties;
+  input: CSSProperties;
+  inputGroup: CSSProperties;
+} = {
   container: {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
-    height: "100vh", // full height of the viewport
+    padding: "20px",
+  },
+  grid: {
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 1fr)",
+    gap: "10px",
+    marginBottom: "20px",
   },
   input: {
     height: "40px",
-    margin: "12px",
     border: "1px solid #000",
     padding: "10px",
-    width: "80%", // Set width as needed
+  },
+  inputGroup: {
+    display: "flex",
+    flexDirection: "column",
   },
 };
 
