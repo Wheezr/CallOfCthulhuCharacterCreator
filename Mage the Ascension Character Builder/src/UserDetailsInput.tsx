2f6c1441fafee98cefc9f2a0677e3539810d252f
Jerry Cheng <chengj12@rpi.edu>
2024-03-18 19:34:20 -0400

    Updated UserDetailsInput component to match the design document more

--- Mage the Ascension Character Builder/src/UserDetailsInput.tsx
+++ Mage the Ascension Character Builder/src/UserDetailsInput.tsx
@@ -3,13 +3,13 @@ import React, { useState, CSSProperties } from "react";
 const UserDetailsInput = () => {
   const [userDetails, setUserDetails] = useState({
     name: "",
-    player: "",
     nature: "",
-    essence: "",
-    demeanor: "",
     tradition: "",
+    player: "",
+    essence: "",
     concept: "",
     chronicle: "",
+    demeanor: "",
     cabal: "",
   });
 
@@ -35,18 +35,6 @@ const UserDetailsInput = () => {
           />
         </div>
 
-        {/* Player */}
-        <div style={styles.inputGroup}>
-          <label htmlFor="player">Player:</label>
-          <input
-            style={styles.input}
-            onChange={(e) => handleInputChange("player", e.target.value)}
-            value={userDetails.player}
-            placeholder="Enter your player"
-            id="player"
-          />
-        </div>
-
         {/* Nature */}
         <div style={styles.inputGroup}>
           <label htmlFor="nature">Nature:</label>
@@ -59,30 +47,6 @@ const UserDetailsInput = () => {
           />
         </div>
 
-        {/* Essence */}
-        <div style={styles.inputGroup}>
-          <label htmlFor="essence">Essence:</label>
-          <input
-            style={styles.input}
-            onChange={(e) => handleInputChange("essence", e.target.value)}
-            value={userDetails.essence}
-            placeholder="Enter your essence"
-            id="essence"
-          />
-        </div>
-
-        {/* Demeanor */}
-        <div style={styles.inputGroup}>
-          <label htmlFor="demeanor">Demeanor:</label>
-          <input
-            style={styles.input}
-            onChange={(e) => handleInputChange("demeanor", e.target.value)}
-            value={userDetails.demeanor}
-            placeholder="Enter your demeanor"
-            id="demeanor"
-          />
-        </div>
-
         {/* Tradition */}
         <div style={styles.inputGroup}>
           <label htmlFor="tradition">Tradition:</label>
@@ -92,6 +56,30 @@ const UserDetailsInput = () => {
             value={userDetails.tradition}
             placeholder="Enter your tradition"
             id="tradition"
+          />
+        </div>
+
+        {/* Player */}
+        <div style={styles.inputGroup}>
+          <label htmlFor="player">Player:</label>
+          <input
+            style={styles.input}
+            onChange={(e) => handleInputChange("player", e.target.value)}
+            value={userDetails.player}
+            placeholder="Enter your player"
+            id="player"
+          />
+        </div>
+
+        {/* Essence */}
+        <div style={styles.inputGroup}>
+          <label htmlFor="essence">Essence:</label>
+          <input
+            style={styles.input}
+            onChange={(e) => handleInputChange("essence", e.target.value)}
+            value={userDetails.essence}
+            placeholder="Enter your essence"
+            id="essence"
           />
         </div>
 
@@ -116,6 +104,18 @@ const UserDetailsInput = () => {
             value={userDetails.chronicle}
             placeholder="Enter your chronicle"
             id="chronicle"
+          />
+        </div>
+
+        {/* Demeanor */}
+        <div style={styles.inputGroup}>
+          <label htmlFor="demeanor">Demeanor:</label>
+          <input
+            style={styles.input}
+            onChange={(e) => handleInputChange("demeanor", e.target.value)}
+            value={userDetails.demeanor}
+            placeholder="Enter your demeanor"
+            id="demeanor"
           />
         </div>
 
@@ -166,3 +166,4 @@ const styles: {
 };
 
 export default UserDetailsInput;
+
