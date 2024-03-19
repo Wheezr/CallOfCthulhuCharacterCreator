c7f58b1f0223cfbbf8af9932ec94d4a3bb5e5144
Jerry Cheng <chengj12@rpi.edu>
2024-03-18 22:07:19 -0400

    update Fonts and Background color

--- Mage the Ascension Character Builder/src/App.tsx
+++ Mage the Ascension Character Builder/src/App.tsx
@@ -3,6 +3,7 @@ import Physical from "./Attributes/Physical";
 import Dot from "./Dot";
 import UserDetailsInput from "./UserDetailsInput";
 import React from "react";
+import "./App.css";
 
 function App() {
   return (
