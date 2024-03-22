bfe59bce1c29506837cda99be2c5ef2ac3b3fe19
Jeremy <jeremy1202chen@gmail.com>
2024-03-22 17:12:50 -0400

    Added Dropdown Menu

--- Mage the Ascension Character Builder/src/App.tsx
+++ Mage the Ascension Character Builder/src/App.tsx
@@ -1,12 +1,35 @@
 import CharacterTraits from "./CharacterTraits";
+import { useState } from "react";
 import Physical from "./Attributes/Physical";
+import Social from "./Attributes/Social";
+import Mental from "./Attributes/Mental";
 import Dot from "./Dot";
 
+let selectionInit = [
+  {value: 'Primary', state: false},
+  {value: 'Secondary', state: false},
+  {value: 'Tertiary', state: false}
+]
+
 function App() {
+  const [selection, changeSelection] = useState(selectionInit);
+  const [selected1, change1] = useState("None")
+
   return (
     <>
       {/* <CharacterTraits /> */}
       { <Physical /> }
+      <h1>{selected1}</h1>
+      <select name="Point Allocate" id="Point Allocate" onChange={(e) => console.log(e.target.disabled)}>
+        <option disabled selected value> -- selection an option --</option>
+        {selection.map((select) => 
+        <option disabled={select.state === true ? true : false} 
+                value={select.value}
+        >{select.value}</option>)}
+      </select>
+      {/* { <Social />}
+      
+      { <Mental />} */}
     </>
   );
 }
