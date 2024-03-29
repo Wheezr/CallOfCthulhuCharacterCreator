40b2857b9fc1d8c623e442b39a0e6df595c90958
Jeremy <jeremy1202chen@gmail.com>
2024-03-29 16:36:00 -0400

    Dropdown disabling (sort of working)

--- Mage the Ascension Character Builder/src/App.tsx
+++ Mage the Ascension Character Builder/src/App.tsx
@@ -1,5 +1,5 @@
 import CharacterTraits from "./CharacterTraits";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import Physical from "./Attributes/Physical";
 import Social from "./Attributes/Social";
 import Mental from "./Attributes/Mental";
@@ -13,14 +13,40 @@ let selectionInit = [
 
 function App() {
   const [selection, changeSelection] = useState(selectionInit);
-  const [selected1, change1] = useState("None")
+  const [selected1, change1] = useState("None");
+
+  // function pressDot(index: number, array: string[], action: Dispatch<SetStateAction<string[]>>) {
+  //   const dot = array.map((c, i) => {
+  //     if (i === index && c === 'dot') {
+  //       return 'undot';
+  //     } else if (i === index && c === 'undot') {
+  //       return 'dot';
+  //     } else {
+  //       return c;
+  //     }
+  //   });
+  //   action(dot);
+  // }
+
+  function change(value: string,
+                  list: {value: string, state: boolean}[],
+                  action: Dispatch<SetStateAction<{value: string, state: boolean}[]>>) {
+    const selects = list.map((item) =>{
+      if (item.value === value) {
+        return {value: value, state: true};
+      } else {
+        return {value: value, state: false};
+      }
+    })
+    action(selects);
+  };
 
   return (
     <>
       {/* <CharacterTraits /> */}
       { <Physical /> }
       <h1>{selected1}</h1>
-      <select name="Point Allocate" id="Point Allocate" onChange={(e) => console.log(e.target.disabled)}>
+      <select name="Point Allocate" id="Point Allocate" onChange={(e) => change(e.target.value, selection, changeSelection)}>
         <option disabled selected value> -- selection an option --</option>
         {selection.map((select) => 
         <option disabled={select.state === true ? true : false} 
