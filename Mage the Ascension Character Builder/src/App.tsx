501be6a8e490c7088a808a33291251529e4bee52
Jeremy <jeremy1202chen@gmail.com>
2024-03-29 16:58:33 -0400

    Fixed broken stuff again

--- Mage the Ascension Character Builder/src/App.tsx
+++ Mage the Ascension Character Builder/src/App.tsx
@@ -9,57 +9,14 @@ import "./App.css";
 import Title from "./Title";
 import CombinedAttributes from "./Attributes/CombinedAttributes";
 
-let selectionInit = [
-  { value: "Primary", state: false },
-  { value: "Secondary", state: false },
-  { value: "Tertiary", state: false },
-];
+
 
 function App() {
-  const [selection, changeSelection] = useState(selectionInit);
-  const [selected1, change1] = useState("None");
-
-  // function pressDot(index: number, array: string[], action: Dispatch<SetStateAction<string[]>>) {
-  //   const dot = array.map((c, i) => {
-  //     if (i === index && c === 'dot') {
-  //       return 'undot';
-  //     } else if (i === index && c === 'undot') {
-  //       return 'dot';
-  //     } else {
-  //       return c;
-  //     }
-  //   });
-  //   action(dot);
-  // }
-
-  function change(value: string,
-                  list: {value: string, state: boolean}[],
-                  action: Dispatch<SetStateAction<{value: string, state: boolean}[]>>) {
-    const selects = list.map((item) =>{
-      if (item.value === value) {
-        return {value: value, state: true};
-      } else {
-        return {value: value, state: false};
-      }
-    })
-    action(selects);
-  };
-
   return (
     <>
-      {/* <CharacterTraits /> */}
-      { <Physical /> }
-      <h1>{selected1}</h1>
-      <select name="Point Allocate" id="Point Allocate" onChange={(e) => change(e.target.value, selection, changeSelection)}>
-        <option disabled selected value> -- selection an option --</option>
-        {selection.map((select) => 
-        <option disabled={select.state === true ? true : false} 
-                value={select.value}
-        >{select.value}</option>)}
-      </select>
-      {/* { <Social />}
-      
-      { <Mental />} */}
+      {<Title />}
+      {<CharacterTraits />}
+      {<CombinedAttributes />}
     </>
   );
 }
--- Mage the Ascension Character Builder/src/Attributes/CombinedAttributes.tsx
+++ Mage the Ascension Character Builder/src/Attributes/CombinedAttributes.tsx
@@ -1,14 +1,44 @@
 import "./CombinedAttributes.css";
+import { useState, Dispatch, SetStateAction } from "react";
 import React from "react";
 import Physical from "./Physical";
 import Social from "./Social";
 import Mental from "./Mental";
 
+let selectionInit = [
+  { value: "Primary", state: false },
+  { value: "Secondary", state: false },
+  { value: "Tertiary", state: false },
+];
+
 function CombinedAttributes() {
+  const [selection, changeSelection] = useState(selectionInit);
+  const [selected1, change1] = useState("None");
+
+  function change(value: string,
+                  list: {value: string, state: boolean}[],
+                  action: Dispatch<SetStateAction<{value: string, state: boolean}[]>>) {
+  const selects = list.map((item) =>{
+    if (item.value === value) {
+      return {value: value, state: true};
+    } else {
+      return {value: value, state: false};
+    }
+  })
+    action(selects);
+  };
   return (
     <>
       <div className="attributegrid">
         <Physical />
+        <h1>{selected1}</h1>
+        <select name="Point Allocate" id="Point Allocate" onChange={(e) => change(e.target.value, selection, changeSelection)}>
+          <option disabled selected value> -- selection an option --</option>
+          {selection.map((select) => 
+          <option disabled={select.state === true ? true : false} 
+                  value={select.value}
+          >{select.value}</option>)}
+        </select>
         <Social />
         <Mental />
       </div>
