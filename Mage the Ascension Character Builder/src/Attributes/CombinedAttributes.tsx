6b5e7dc74f85e8ff0add1efcd47b6d1c5d1f75c6
Jerry Cheng <chengj12@rpi.edu>
2024-04-05 20:51:18 -0400

    Prepping for webpage scaling

--- Mage the Ascension Character Builder/src/Attributes/CombinedAttributes.tsx
+++ Mage the Ascension Character Builder/src/Attributes/CombinedAttributes.tsx
@@ -15,32 +15,53 @@ function CombinedAttributes() {
   const [selection, changeSelection] = useState(selectionInit);
   const [selected1, change1] = useState("None");
 
-  function change(value: string,
-                  list: {value: string, state: boolean}[],
-                  action: Dispatch<SetStateAction<{value: string, state: boolean}[]>>) {
-  const selects = list.map((item) =>{
-    if (item.value === value) {
-      return {value: value, state: true};
-    } else {
-      return {value: value, state: false};
-    }
-  })
+  function change(
+    value: string,
+    list: { value: string; state: boolean }[],
+    action: Dispatch<SetStateAction<{ value: string; state: boolean }[]>>
+  ) {
+    const selects = list.map((item) => {
+      if (item.value === value) {
+        return { value: value, state: true };
+      } else {
+        return { value: value, state: false };
+      }
+    });
     action(selects);
-  };
+  }
   return (
     <>
       <div className="attributegrid">
-        <Physical />
-        <h1>{selected1}</h1>
-        <select name="Point Allocate" id="Point Allocate" onChange={(e) => change(e.target.value, selection, changeSelection)}>
-          <option disabled selected value> -- selection an option --</option>
-          {selection.map((select) => 
-          <option disabled={select.state === true ? true : false} 
-                  value={select.value}
-          >{select.value}</option>)}
-        </select>
-        <Social />
-        <Mental />
+        <div className="attributebox">
+          <Physical />
+          <h1>{selected1}</h1>
+          <select
+            name="Point Allocate"
+            id="Point Allocate"
+            onChange={(e) => change(e.target.value, selection, changeSelection)}
+          >
+            <option disabled selected value>
+              {" "}
+              -- selection an option --
+            </option>
+            {selection.map((select) => (
+              <option
+                disabled={select.state === true ? true : false}
+                value={select.value}
+              >
+                {select.value}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="attributebox">
+          <Social />
+        </div>
+
+        <div className="attributebox">
+          <Mental />
+        </div>
       </div>
     </>
   );
